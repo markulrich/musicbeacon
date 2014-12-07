cc3a093aa51c1f0ec169eef331e5f14de1c8334d
Mark Ulrich <mulrich@stanford.edu>
2014-12-06 22:09:43 -0800

    Added time testing and improved peertime

--- src/peer-time.js
+++ src/peer-time.js
@@ -1,84 +1,126 @@
-/**
+/* *
  * Module to synchronize node times with a master server.
  * Nodes periodically ping the pubnub server and compute the drift based on historical latencies.
  */
 
-var PeerTime = (function() {
-  function PeerTime(pubnub, mode) {
-    this.mode = mode || 'moving';
-    this.drift = 0;
-    this.drifts = [];
-    this.pubnub = pubnub;
-    this.numSyncs = 0;
+'use strict';
 
-    if (!(this.mode in this.driftAlgos)) {
-      throw new Error('Invalid mode', this.mode);
-    }
+function PeerTime(pubnub, mode) {
+  this.mode = mode || 'weighted';
+  this.drift = 0;
+  this.drifts = [];
+  this.pubnub = pubnub;
+  this.numSyncs = 0;
 
-    var self = this;
-    window.setInterval(function() {
-      self.syncDrift();
-    }, this.REFRESH);
+  if (!(this.mode in this.driftAlgos)) {
+    throw new Error('Invalid mode', this.mode);
+    this.mode = 'none';
   }
 
-  PeerTime.prototype = {
-    REFRESH: 2000,
-    MAX_TIMEOUT: 5000,
-    TENTHS_NS_PER_MS: 10000,
-    MOVING_WINDOW: 60,
-    EWMA_DECAY: 0.90,
+  window.setInterval(function () {
+    console.log('SYNCING!!');
+    this.syncDrift();
+  }.bind(this), this.REFRESH);
+}
 
-    driftAlgos: {
-      total: function(currDrift) {
-        this.drifts.push(currDrift);
-      },
-      moving: function(currDrift) {
-        this.drifts.push(currDrift);
+PeerTime.prototype = {
+  REFRESH: 1500,
+  MAX_RTT: 500,
+  TENTHS_NS_PER_MS: 10000,
+  TAKE_BEST: 4,
+  MOVING_WINDOW: 10,
+  EWMA_DECAY: 0.4,
+
+  avgOfKey: function(arr, key) {
+    var sum = 0, len = arr.length;
+    for (var i = 0; i < len; i++) {
+      sum += arr[i][key]
+    }
+    return sum / len;
+  },
+
+  driftAlgos: {
+    lowRTT: function(currDrift, roundTripTime) {
+        this.drifts.push({offset: currDrift, rtt: roundTripTime});
         if (this.drifts.length > this.MOVING_WINDOW) this.drifts.shift();
-        this.drift = this.avgDrift();
-      },
-      exponential: function(currDrift) {
-        if (this.numSyncs === 0) this.drift = currDrift;
-        return this.EWMA_DECAY * this.drift + (1 - this.EWMA_DECAY) * currDrift;
-      },
-      none: function() {
-        return 0;
-      }
+        var drifts = this.drifts.slice(0);
+        drifts.sort(function(a, b) { return a.rtt - b.rtt; });
+        drifts = drifts.slice(0, this.TAKE_BEST);
+        this.drift = this.avgOfKey(drifts, 'offset');
     },
+    highRTT: function(currDrift, roundTripTime) {
+        this.drifts.push({offset: currDrift, rtt: roundTripTime});
+        if (this.drifts.length > this.MOVING_WINDOW) this.drifts.shift();
+        var drifts = this.drifts.slice(0);
+        drifts.sort(function(a, b) { return b.rtt - a.rtt; });
+        drifts = drifts.slice(0, this.TAKE_BEST);
+        this.drift = this.avgOfKey(drifts, 'offset');
+    },
+    minRTT: function(currDrift, roundTripTime) {
+        this.drifts.push({offset: currDrift, rtt: roundTripTime});
+        if (this.drifts.length > this.MOVING_WINDOW) this.drifts.shift();
+        var drifts = this.drifts.slice(0);
+        drifts.sort(function(a, b) { return a.rtt - b.rtt; });
+        this.drift = drifts[0].offset;
+    },
+    maxRTT: function(currDrift, roundTripTime) {
+        this.drifts.push({offset: currDrift, rtt: roundTripTime});
+        if (this.drifts.length > this.MOVING_WINDOW) this.drifts.shift();
+        var drifts = this.drifts.slice(0);
+        drifts.sort(function(a, b) { return b.rtt - a.rtt; });
+        this.drift = drifts[0].offset;
+    },
+    total: function(currDrift) {
+        this.drifts.push(currDrift);
+        this.drift = this.avgDrift();
+    },
+    none: function() {
+        this.drift = 0;
+    },
+    curr: function(currDrift) {
+      this.drift = currDrift;
+    },
+    weighted: function(currDrift) {
+      if (this.numSyncs === 0) this.drift = currDrift;
+      this.drift = this.EWMA_DECAY * this.drift + (1 - this.EWMA_DECAY) * currDrift;
+    }
+  },
 
-    syncDrift: function() {
-      var self = this;
-      var startTime = new Date().getTime();
-      this.pubnub.time(
-        function(serverTimeTenthsNs) {
-          var serverTime = serverTimeTenthsNs / self.TENTHS_NS_PER_MS;
+  syncDrift: function () {
+    var startTime = new Date().getTime();
+    this.pubnub.time(
+        function (serverTimeTenthsNs) {
+          var serverTime = serverTimeTenthsNs / this.TENTHS_NS_PER_MS;
           if (serverTime === 0) {
-            // console.error("Failed to return server.time result");
+            console.error('Failed to return server.time result');
             return;
           }
 
           var currTime = new Date().getTime();
           var roundTripTime = currTime - startTime;
-          if (roundTripTime > self.MAX_TIMEOUT) {
+          if (roundTripTime > this.MAX_RTT) {
             console.error('Latency too high to compute drift:', roundTripTime);
-            return;
+            if (this.numSyncs > 0) {
+              return;
+            }
           }
 
           var currDrift = serverTime - currTime + roundTripTime / 2;
-          self.driftAlgos[self.mode].call(self, currDrift);
-          self.numSyncs++;
-        }
-      );
-    },
+          this.driftAlgos[this.mode].call(this, currDrift, roundTripTime);
+          this.numSyncs++;
+        }.bind(this)
+    );
+  },
 
-    avgDrift: function() {
-      return _.reduce(this.drifts, function(a, b) { return a + b; }) / this.drifts.length;
-    },
+  avgDrift: function () {
+    return _.reduce(this.drifts, function(a, b) { return a + b; }) / this.drifts.length;
+  },
 
-    currTime: function() {
-      return new Date().getTime() + this.drift;
-    }
-  };
+  currTime: function () {
+    return new Date(new Date().getTime() + this.drift);
+  },
 
-  return PeerTime;
-})();
+  currDrift: function() {
+    return this.drift;
+  }
+};
